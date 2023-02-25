5a1c4e66078c48e9161fc89ca4fdaea360c97d77
Javier9722 <eduardo.javier.rf@gmail.com>
2023-02-24 21:15:50 -0500

    edit astro config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://javier9722.github.io",
+  base: "/fruitPage",
   integrations: [
     tailwind({
       config: { applyBaseStyles: true },
